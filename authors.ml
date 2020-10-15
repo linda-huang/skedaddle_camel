92108c2787439b957a92743bb7a91ec402e497ea
ae347@cornell.edu <annaeffen@LAPTOP-O482FQLI.localdomain>
2020-10-15 19:32:00 -0400

    authors unit

---
+++ authors.ml
@@ -0,0 +1,3 @@
+let net_ids = ["ae347"; "ddt45"; "sh837"; "tl676"]
+
+let hours_worked = [0; 0; 0; 0]
---
+++ authors.mli
@@ -0,0 +1,5 @@
+(** [net_ids] is the list of team members' net ids *)
+val net_ids : string list 
+(** [hours_worked] is the number of hours worked on this assignment 
+    by each team member, in the order the [net_ids]. *)
+val hours_worked : int list
