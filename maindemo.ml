8ef919ac0189a0581231516c55fdb9f3aed508a7
ae347@cornell.edu <annaeffen@LAPTOP-O482FQLI.localdomain>
2020-10-19 23:15:00 -0400

    Update maindemo.ml multiple key presses work to change location of camel

--- maindemo.ml
+++ maindemo.ml
@@ -8,18 +8,31 @@ open Unix
 
 let draw_state state = failwith "todo" 
 
-let init k =  
+let input (camel : Camel.t) =  
   Graphics.clear_graph ();
+  Graphics.moveto 50 500;
+  Graphics.draw_string "press a key to move";
+  let k = Graphics.read_key () in 
+  match k with 
+  | '0' -> exit 0 
+  | 'w' -> (Camel.move_vert camel 1.)
+  | 'a' -> (Camel.move_horiz camel ~-.1.)
+  | 's' -> (Camel.move_vert camel ~-.1.)
+  | 'd' -> (Camel.move_horiz camel 1.)
+  | _ -> camel 
+
+let rec run (camel : Camel.t) = 
+  Graphics.clear_graph ();
+  Graphics.moveto 50 500;
+  let movedcamel = input camel in 
+  Graphics.draw_string ("Began as: " ^ Camel.string_of_camel camel);
+  Graphics.draw_string ("\nMoved to: " ^ Camel.string_of_camel movedcamel);
+  Unix.sleep 1;
+  run movedcamel 
+
+let init k = 
   let camel = Camel.init 0. 0. in 
-  let movedcamel = 
-    match k with 
-    | 'w' -> (Camel.move_vert camel 1.)
-    | 'a' -> (Camel.move_horiz camel ~-.1.)
-    | 's' -> (Camel.move_vert camel ~-.1.)
-    | 'd' -> (Camel.move_horiz camel 1.)
-    | _ -> camel 
-  in Graphics.draw_string ("Began as: " ^ Camel.string_of_camel camel);
-  Graphics.draw_string ("\nMoved to: " ^ Camel.string_of_camel movedcamel)
+  run camel 
 
 (*draw_state State.init_state; run State.init_state*)
 
@@ -29,10 +42,10 @@ let main () =
   Graphics.resize_window 800 800;
   Graphics.set_text_size 300;
   Graphics.moveto 50 500;
-  Graphics.draw_string "press a key to move";
+  Graphics.draw_string "press a key to start";
   match Graphics.read_key () with 
   | k -> init k 
 
 (* Execute the demo. *)
-let () = main (); 
-  Unix.sleep 5
+let () = main ()
+(*https://stackoverflow.com/questions/36263152/simple-ocaml-graphics-progam-that-close-before-its-window-is-displayed *)
